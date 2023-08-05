474d5f37d78e893e750b4b51667965d28862e221
Taexee <hee66265@naver.com>
2023-08-06 04:47:22 +0900

    api

--- src/main/java/hello/hellospring/controller/HelloContoller.java
+++ src/main/java/hello/hellospring/controller/HelloContoller.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloContoller {
@@ -17,5 +18,32 @@ public class HelloContoller {
     public String helloMvc(@RequestParam(value = "name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http 프로토콜의 body를 의미
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // "hello name" view없이 http 프로토콜 body로 직접 넘겨줌
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; // 객체를 반환
+        // json형식으로 변환
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
