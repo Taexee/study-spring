fb96d8f019f1c786cd439b5d36b0ae7578b720e0
Taexee <hee66265@naver.com>
2023-08-06 05:53:27 +0900

    회원 도메인과 리포지토리 만들기

---
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    // optional null을 반환할때 Optional로 감싸서 반환
+    List<Member> findAll();
+
+}
