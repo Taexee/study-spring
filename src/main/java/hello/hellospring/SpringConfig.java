d98631491f08ada58bbd3992ddc20bf359e2433e
Taexee <hee66265@naver.com>
2023-08-13 16:38:56 +0900

    스프링 JDBC Template

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,6 +1,7 @@
 package hello.hellospring;
 
 import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
@@ -28,7 +29,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-//      return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateRepository(dataSource);
     }
 }
